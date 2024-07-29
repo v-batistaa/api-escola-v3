7c0cd594a6e92f7a740da34f5b9565748e0d5958
Vinicius Gabriel <viniciusrtycbn@gmail.com>
2024-07-29 19:35:38 -0300

    first commit

---
+++ escola/views.py
@@ -0,0 +1,35 @@
+from escola.models import Estudante,Curso, Matricula
+from escola.serializers import EstudanteSerializer,CursoSerializer, MatriculaSerializer, ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer, EstudanteSerializerV2
+from rest_framework import viewsets, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    # serializer_class = EstudanteSerializer
+    filter_backends = [DjangoFilterBackend,filters.OrderingFilter,filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome','cpf']
+    def get_serializer_class(self):
+        if self.request.version == 'v2':
+            return EstudanteSerializerV2
+        return EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculaEstudante(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializer
+
+class ListaMatriculaCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
